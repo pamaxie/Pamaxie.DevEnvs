a87daaf00b165cae7c1fafbc5813a71c3c99d66a
LiaAiluri <0238410-github@lia.vc>
2022-11-23 07:38:47 +0100

    #1 Added Database API Dev Environment
    Created Docker Deploy Script for our Database Dev Environment.

--- src/Database.Env/start.sh
+++ src/Database.Env/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#Start our postgres server
-/usr/lib/postgresql/12/bin/postgres -D /var/lib/postgresql/12/main -c config_file=/etc/postgresql/12/main/postgresql.conf
-
 #Start our redis server
 redis-server /etc/redis/redis.conf
+
+#Start our postgres server
+su - postgres -c '/usr/lib/postgresql/12/bin/postgres -D /var/lib/postgresql/12/main'
