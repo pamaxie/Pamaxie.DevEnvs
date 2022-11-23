6bbc477d012ce0d60b0d70c3eea5328ff966fb6b
LiaAiluri <0238410-github@lia.vc>
2022-11-23 08:49:51 +0100

    #1 Added better base image for C# development

--- src/Database.Env/start.sh
+++ src/Database.Env/start.sh
@@ -3,5 +3,7 @@
 #Start our redis server
 redis-server /etc/redis/redis.conf
 
+bash /data/configure_redis.sh
+
 #Start our postgres server
 su - postgres -c '/usr/lib/postgresql/12/bin/postgres -D /var/lib/postgresql/12/main'
